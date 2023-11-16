cf39dcb6ed9beb794c00aead5394bb0e7394b984
micANDREW <101782917+micANDREW@users.noreply.github.com>
2023-11-16 00:52:20 -0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,133 @@
+const images = document.querySelectorAll('#slider img');
+const previousImage = document.getElementById("prev");
+const nextImage = document.getElementById("next");
+
+let currentIndex = 0;
+
+function reset() {
+  for (let i = 0; i < images.length; i++) {
+    images[i].classList.remove('active');
+  }
+}
+
+function initializeSlider() {
+  reset();
+  images[currentIndex].classList.add('active');
+}
+
+function slideLeft() {
+  reset();
+  currentIndex--;
+  if (currentIndex < 0) {
+    currentIndex = images.length - 1;
+  }
+  images[currentIndex].classList.add('active');
+}
+
+function slideRight() {
+  reset();
+  currentIndex++;
+  if (currentIndex >= images.length) {
+    currentIndex = 0;
+  }
+  images[currentIndex].classList.add('active');
+}
+
+initializeSlider();
+
+previousImage.addEventListener('click', function() {
+  slideLeft();
+});
+
+nextImage.addEventListener('click', function() {
+  slideRight();
+});
+
+//contact js
+// Get all the necessary DOM elements
+const form = document.getElementById('contactForm')
+const submitButton = document.querySelector('submitBtn')
+const successMessage = document.getElementById('successMessage')
+// Store all form elements in an array by spreading the elements property of the form
+const formElements = [ ...form.elements ]
+// Create function to check if all form elements are valid
+const allInputsValid = () => {
+  const valid = formElements.every((element) => {
+    if (element.nodeName === 'SELECT') {
+      return element.value !== 'Please select an option';
+    } else {
+      return element.checkValidity();
+    }
+  });
+
+  return valid;
+};
+// Define a function to handle changes to any form element
+const handleChange = () => {
+  // Use the forEach() function to execute the provided function once for each element in the formElements array
+  formElements.forEach((element) => {
+    // If the element is invalid and is not a button, a select dropdown, a checkbox, or a radio button, style it with a red border and red text
+    if (!element.checkValidity())
+           {
+      element.style.borderColor = 'red'
+      element.nextElementSibling.style.color = 'red'
+      element.nextElementSibling.style.display = 'block'
+      element.previousElementSibling.style.color = 'red'
+    }
+
+    // If the element is valid, reset its style to the original colors
+    // The conditions are the same as above for excluding certain elements
+    if (element.checkValidity())
+          {
+      element.style.borderColor = '#CED4DA'
+      element.nextElementSibling.style.color = '#CED4DA'
+      element.nextElementSibling.style.display = 'none'
+      element.previousElementSibling.style.color = '#212529'
+    }
+
+    // If the element is a checkbox or a radio button and is invalid, style it with a red border and red text
+    if (!element.checkValidity()
+    ) {
+      element.style.borderColor = 'red'
+      element.nextElementSibling.style.color = 'red'
+    }
+
+    // If the checkbox or radio button is valid, reset its style to the original colors
+    if (element.checkValidity()
+         
+    ) {
+      element.style.borderColor = '#CED4DA'
+      element.nextElementSibling.style.color = '#212529'
+    }
+
+    // If all form elements are valid, enable the submit button; otherwise, disable it
+  if (allInputsValid()) {
+    submitButton.removeAttribute('disabled', '')
+  } else {
+    submitButton.setAttribute('disabled', '')
+  }
+}
+// Define a function to handle form submission
+const handleSubmit = (e) => {
+  // Prevent the default form submission behavior
+  e.preventDefault()
+
+  // If all form elements are valid after the form submission, display a success message, reset the form, and disable the submit button
+  if (allInputsValid()) {
+    successMessage.style.display = 'block'
+    form.reset()
+    submitButton.setAttribute('disabled', '')
+
+    // Hide the success message after 3 seconds
+    setTimeout(() => {
+      successMessage.style.display = 'none'
+    }, 3000)
+  }
+}
+// Add event listener to each form element
+formElements.forEach((element) => {
+  element.addEventListener('change', handleChange)
+})
+
+// Add submit listener to the form
+form.addEventListener('submit', (e) => handleSubmit(e))
